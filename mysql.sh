c4b03f981c132675c5f2e9da70338f5f6077aef0
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-05-03 18:49:38 +0530

    array script commit

---
+++ mysql.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+VALIDATE(){
+    # echo "Exist status: $1"
+    # echo "What are you doing : $2"
+    if [ $1 -ne 0 ]
+    then
+        echo "$2..$R FAILURE $N"
+        exit 1
+    else
+        echo "$2..$R SUCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+# Install MySQL Server 8.0.x
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySql server"
+
+# Start MySQL Service
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling MySql server"
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Start MySql server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+VALIDATE $? "Setting up root  MySql passwd"
